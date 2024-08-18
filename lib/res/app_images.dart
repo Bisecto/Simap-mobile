73d996da6484d7f35213f52318e818cf9c390355
precious <bisector0@gmail.com>
2024-08-19 00:48:47 +0100

    Feature: Added splash screen

--- lib/res/app_images.dart
+++ lib/res/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const String _imageBaseUrl = 'assets/images/';
 
   static const signal = '${_imageBaseUrl}signa.png';
+  static const appleadLogo = '${_imageBaseUrl}appleadLogo.png';
 }
---
+++ lib/view/app_screens/splash_screen/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../../../res/app_colors.dart';
+import '../../../res/app_images.dart';
+import '../../../utills/app_utils.dart';
+import '../../widgets/app_custom_text.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  AppUtils appUtils = AppUtils();
+
+  @override
+  void initState() {
+    //appUtils.openApp(context);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: AppUtils.deviceScreenSize(context).height,
+      width: AppUtils.deviceScreenSize(context).width,
+      decoration: const BoxDecoration(color: AppColors.white
+          // image: DecorationImage(
+          //   image: AssetImage(AppImages.splashFrame,),fit: BoxFit.fill
+          // ),
+
+          ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CustomText(text: ''),
+          // Center(
+          //     child: Image.asset(
+          //   AppImages.logo,
+          //   height: 150,
+          //   width: 150,
+          // )),
+          TextStyles.textHeadings(textValue: "SIMAP",textSize: 24),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              children: [
+                const CustomText(text: 'Powered by', color: AppColors.black),
+                Image.asset(
+                  AppImages.appleadLogo,
+                  height: 80,
+                  width: 150,
+                  //color: AppColors.darkModeBlack,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
